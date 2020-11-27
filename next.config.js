dbe8a83322b6d53514e0ada262e66e6bc59f8c62
Markus Wendorf <markuswendorf@web.de>
2020-11-27 18:39:02 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,28 +1,5 @@
 /* eslint-disable */
 const withCSS = require("@zeit/next-css");
 const production = process.env.NODE_ENV === "production";
-const path = require("path");
 
-const isGHAMaster = process.env.GITHUB_REF && process.env.GITHUB_REF.includes("master");
-const isGHAStage = process.env.GITHUB_REF && process.env.GITHUB_REF.includes("development");
-//const isDev = !process.env.GITHUB_REF
-const stage = isGHAMaster ? "prod" : isGHAStage ? "stage" : "dev";
-
-const baseUrls = {
-  prod: "https://signalsofhope.buchmesse.de",
-  stage: "https://stage-signalsofhope.buchmesse.de",
-  dev: "http://localhost:3000",
-};
-const backendUrls = {
-  prod: "https://signalsofhope.buchmesse.de",
-  stage: "https://stage-signalsofhope.buchmesse.de", //"https://stage-signalsofhope.buchmesse.de",
-  dev: "https://stage-signalsofhope.buchmesse.de", //"http://localhost:1337"
-};
-
-module.exports = withCSS({
-  env: {
-    FRONTEND_URL: baseUrls[stage],
-    BACKEND_URL: backendUrls[stage],
-    STAGE: stage,
-  },
-});
+module.exports = withCSS({});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   purge: ["./**/*.html", "./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      xxl: "1440px",
-    },
     flex: {
       1: "1 1 0%",
       2: "2 2 0%",
@@ -15,10 +8,6 @@ module.exports = {
       4: "4 4 0%",
     },
     extend: {},
-  },
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
   },
   variants: {},
   plugins: [],
